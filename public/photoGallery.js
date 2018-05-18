d7106995944ae65dfaa18c051ef6af68846410b4
hshidara <hshidara@ucdavis.edu>
2018-05-18 23:10:19 +0000

    added nina's photo gallery code

---
+++ public/photoGallery.js
@@ -0,0 +1,127 @@
+
+/* This array is just for testing purposes.  You will need to 
+   get the real image data using an AJAX query. */
+
+const photos = [
+{src: "http://lotus.idav.ucdavis.edu/public/ecs162/UNESCO/A%20Torre%20Manuelina.jpg", width: 574, height: 381 },
+{src: "http://lotus.idav.ucdavis.edu/public/ecs162/UNESCO/Uluru%20sunset1141.jpg", width: 500 , height: 334 },
+{src: "http://lotus.idav.ucdavis.edu/public/ecs162/UNESCO/Sejong tomb 1.jpg", width: 574, height: 430},
+{src: "http://lotus.idav.ucdavis.edu/public/ecs162/UNESCO/Serra%20da%20Capivara%20-%20Painting%207.JPG", width: 574, height: 430},
+{src: "http://lotus.idav.ucdavis.edu/public/ecs162/UNESCO/Royal%20Palace%2c%20Rabat.jpg", width: 574, height: 410},
+{src: "http://lotus.idav.ucdavis.edu/public/ecs162/UNESCO/Red%20pencil%20urchin%20-%20Papahnaumokukea.jpg", width: 574 , height: 382 }
+];
+
+
+
+// A react component for a tag
+class Tag extends React.Component {
+
+    render () {
+	return React.createElement('p',  // type
+	    { className: 'tagText'}, // properties
+	   this.props.text);  // contents
+    }
+};
+
+
+// A react component for controls on an image tile
+class TileControl extends React.Component {
+
+    render () {
+	// remember input vars in closure
+        var _selected = this.props.selected;
+        var _src = this.props.src;
+        // parse image src for photo name
+	var photoName = _src.split("/").pop();
+	photoName = photoName.split('%20').join(' ');
+
+        return ( React.createElement('div', 
+ 	 {className: _selected ? 'selectedControls' : 'normalControls'},  
+         // div contents - so far only one tag
+              React.createElement(Tag,
+		 { text: photoName })
+	    )// createElement div
+	)// return
+    } // render
+};
+
+
+// A react component for an image tile
+class ImageTile extends React.Component {
+
+    render() {
+	// onClick function needs to remember these as a closure
+	var _onClick = this.props.onClick;
+	var _index = this.props.index;
+	var _photo = this.props.photo;
+	var _selected = _photo.selected; // this one is just for readability
+
+	return (
+	    React.createElement('div', 
+	        {style: {margin: this.props.margin, width: _photo.width},
+			 className: 'tile',
+                         onClick: function onClick(e) {
+			    console.log("tile onclick");
+			    // call Gallery's onclick
+			    return _onClick (e, 
+					     { index: _index, photo: _photo }) 
+				}
+		 }, // end of props of div
+		 // contents of div - the Controls and an Image
+		React.createElement(TileControl,
+		    {selected: _selected, 
+		     src: _photo.src}),
+		React.createElement('img',
+		    {className: _selected ? 'selected' : 'normal', 
+                     src: _photo.src, 
+		     width: _photo.width, 
+                     height: _photo.height
+			    })
+				)//createElement div
+	); // return
+    } // render
+} // class
+
+
+
+// The react component for the whole image gallery
+// Most of the code for this is in the included library
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { photos: photos };
+    this.selectTile = this.selectTile.bind(this);
+  }
+
+  selectTile(event, obj) {
+    console.log("in onclick!", obj);
+    let photos = this.state.photos;
+    photos[obj.index].selected = !photos[obj.index].selected;
+    this.setState({ photos: photos });
+  }
+
+  render() {
+    return (
+       React.createElement( Gallery, {photos: photos, 
+		   onClick: this.selectTile, 
+		   ImageComponent: ImageTile} )
+	    );
+  }
+
+}
+
+/* Finally, we actually run some code */
+
+const reactContainer = document.getElementById("react");
+
+ReactDOM.render(React.createElement(App),reactContainer);
+
+
+
+
+
+
+
+
+
